342d47806bd0f27d38d167a304f37de9b39d401b
ZotovaLera <90097489+Ler4i@users.noreply.github.com>
2021-11-07 17:54:07 +0300

    Добавили профиль

--- src/router/index.js
+++ src/router/index.js
@@ -44,7 +44,22 @@ export const constantRoutes = [
   },
 
   {
-    path: '/',
+    path: '/profile',
+    component: Layout,
+    redirect: '/profile/index',
+    hidden: true,
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/profile/index'),
+        name: 'Profile',
+        meta: { title: 'Profile', icon: 'user', noCache: true }
+      }
+    ]
+  },
+
+  {
+    path: '/',//главная стр
     component: Layout,
     redirect: '/dashboard',
     children: [{
@@ -170,7 +185,7 @@ export const asyncRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
